d4c3dd87133acba908cf37d081fe01e3dd18d56a
Raphael <raphael-gfigueiredo@hotmail.com>
2024-07-11 09:13:07 +0200

    CRIANDO LAYOUT

--- app/src/main/java/com/example/calcularimc/MainActivity.kt
+++ app/src/main/java/com/example/calcularimc/MainActivity.kt
@@ -1,20 +1,62 @@
 package com.example.calcularimc
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Recuperar componentes EditText
+        // Criar uma varíavel e associar o componente de UI <EditText>
+        // Recuperar o botao da tela
+
+        // Colocar acao no botao setOnclickListener
+        // Recuperar o texto digitado no edt peso
+
+
+        val edtpeso = findViewById<TextInputEditText>(R.id.edittext_peso)
+        val edtaltura = findViewById<TextInputEditText>(R.id.edittext_altura)
+
+        val btnCalcular = findViewById<Button>(R.id.btn_Calcular)
+
+
+        btnCalcular.setOnClickListener {
+
+
+            val pesoStr: String = edtpeso.text.toString()
+            val alturaStr: String = edtaltura.text.toString()
+
+            if (pesoStr == "" || alturaStr == "") {
+                // Mostrar mensagem para o usuario
+
+                Snackbar.make(
+                    edtpeso,
+                    "Preencha todos os campos",
+                    Snackbar.LENGTH_LONG
+                )
+                    .show()
+
+            } else {
+
+
+                val peso = pesoStr.toFloat()
+                val altura = alturaStr.toFloat()
+
+
+                val alturaQ2 = altura * altura
+                val resultado = peso / alturaQ2
+
+
+                println("Raphael Acao do botao" + resultado)
+            }
+
+
         }
     }
 }
+
